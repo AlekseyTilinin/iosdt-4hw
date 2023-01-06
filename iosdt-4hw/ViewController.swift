9e4bf1efc20be9d65638d54956af8b99b3082395
AlekseyTilinin <alexeytilinin@yandex.ru>
2023-01-07 00:27:16 +0300

    Initial Commit

---
+++ iosdt-4hw/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iosdt-4hw
+//
+//  Created by Aleksey on 07.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
